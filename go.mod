f3b35263183842ca38a7a03bb3a3759c5811ed3a
Arttu Kaipainen <arttu@nanjo.dev>
2026-03-28 16:26:49 +0200

    feat: Automatically disable colored output for specific environments

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/IamNanjo/go-logging
 
 go 1.26.0
 
-require golang.org/x/sys v0.41.0
+require (
+	golang.org/x/sys v0.42.0
+	golang.org/x/term v0.41.0
+)
 
 require (
 	golang.org/x/mod v0.33.0 // indirect
--- pkg/ansi/coloredtext.go
+++ pkg/ansi/coloredtext.go
@@ -1,12 +1,23 @@
 package ansi
 
+import (
+	"os"
+
+	"golang.org/x/term"
+)
+
 type ColoredText struct {
 	Color Color
 	Text  string
 }
 
+var useColor = term.IsTerminal(int(os.Stdout.Fd())) && os.Getenv("NO_COLOR") == "" && os.Getenv("TERM") != "dumb"
+
+// Output string with ANSI escape codes unless output is not a terminal,
+// user has NO_COLOR environment variable set or
+// TERM environment variable is set to dumb.
 func (ct *ColoredText) String() string {
-	if ct.Color == None {
+	if !useColor || ct.Color == None {
 		return ct.Text
 	}
 	return string(ct.Color) + ct.Text + string(Reset)
