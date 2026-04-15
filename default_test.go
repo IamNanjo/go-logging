86005154ac4225a46124df3f5c3e737d3ec02241
Arttu Kaipainen <arttu@nanjo.dev>
2026-04-15 19:42:45 +0300

    test: Fix test for CI

--- default_test.go
+++ default_test.go
@@ -21,11 +21,21 @@ func TestOut(t *testing.T) {
 }
 
 func TestDebug(t *testing.T) {
+	loglevel.Level = loglevel.INFO
 	bytesWritten, err := l.Debug("Default debug logging with LOGLEVEL set to %s\n", loglevel.Level)
 	if err != nil {
 		t.Fatalf("Logging failed: %v", err)
 	}
-	if bytesWritten == 0 {
+	if bytesWritten != 0 {
+		t.Fatalf("Bytes were written regardless of log level")
+	}
+
+	loglevel.Level = loglevel.DEBUG
+	bytesWritten, err = l.Debug("Default debug logging with LOGLEVEL set to %s\n", loglevel.Level)
+	if err != nil {
+		t.Fatalf("Logging failed: %v", err)
+	}
+	if bytesWritten != 0 {
 		t.Fatalf("No bytes were written")
 	}
 }
